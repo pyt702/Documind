67d8501751b981ccd87a42cf5f9f864baed2efc9
Bunny-learner <ambatikalyani1582@gmail.com>
2026-07-03 00:37:28 +0530

    final ones

--- frontend/src/components/PdfViewerModal.jsx
+++ frontend/src/components/PdfViewerModal.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 // Set up worker
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
